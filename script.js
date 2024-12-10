42b2b91e8ea0c5fc90e27ff73d5a2109005acc07
Sapar-s <smagulsapar52@gmail.com>
2024-12-10 12:47:59 +0800

    oilgoh heregtei

--- script.js
+++ script.js
@@ -51,65 +51,97 @@ const containerItems = [
 ];
 
 const todoData = [
-  { title: "geree tseverleh", date: "2024-12-09", state: "todo" },
-  { title: "shine jildee yvah", date: "2024-12-09", state: "todo" },
-  { title: "huvtsaa beldeh", date: "2024-12-09", state: "todo" },
-  { title: "ochih", date: "2024-12-09", state: "todo" },
+  { title: "geree tseverleh", date: "12-09", state: "to-do" },
+  { title: "shine jildee yvah", date: "12-09", state: "to-do" },
+  { title: "huvtsaa beldeh", date: "12-09", state: "to-do" },
+  // { title: "ochih", date: "2024-12-09", state: "to-do" },
+  { title: "geree tseverleh", date: "12-10", state: "in-progress" },
+  { title: "shine jildee yvah", date: "12-10", state: "in-progress" },
+  { title: "huvtsaa beldeh", date: "12-10", state: "in-progress" },
+  // { title: "ochih", date: "2024-12-09", state: "to-do" },
+  { title: "geree tseverleh", date: "12-11", state: "done" },
+  { title: "shine jildee yvah", date: "12-11", state: "done" },
+  { title: "huvtsaa beldeh", date: "12-11", state: "done" },
+  // { title: "ochih", date: "2024-12-09", state: "to-do" },
+  { title: "geree tseverleh", date: "12-12", state: "blocked" },
+  { title: "shine jildee yvah", date: "12-12", state: "blocked" },
+  { title: "huvtsaa beldeh", date: "12-12", state: "blocked" },
+  // { title: "ochih", date: "2024-12-09", state: "todo" },
 ];
+
+const inData = [];
+
+const doneData = [];
+
+const blockedData = [];
 
 function addTaskList(title, color, count, id) {
   const taskContainer2 = document.querySelector("#taskContainer");
   const todoList = document.createElement("div");
-  todoList.setAttribute("class", "todoList");
+  todoList.setAttribute("id", id);
+
   const titleDiv = document.createElement("div");
   titleDiv.setAttribute("class", "title");
   todoList.appendChild(titleDiv);
+
   const h2 = document.createElement("h2");
   h2.innerText = title;
+
   const cirlce = document.createElement("div");
   cirlce.setAttribute("class", "circle");
   cirlce.style.backgroundColor = color;
   titleDiv.appendChild(cirlce);
   titleDiv.appendChild(h2);
   taskContainer2.appendChild(todoList);
-
-  // <p class="count"></p>
-  const para = document.createElement("p");
-  para.setAttribute("class", "count");
-  para.innerText = count;
-
-  const list = document.createElement("div");
-  list.setAttribute("class", "list");
-  todoList.appendChild(list);
-  const listItem = document.createElement("div");
-  listItem.setAttribute("class", "listItem");
-  list.appendChild(listItem);
-  const p = document.createElement("p");
-  listItem.appendChild(p);
-
-  const select = document.createElement("select");
-  listItem.appendChild(select);
-  const option = document.createElement("option");
-  const option1 = document.createElement("option");
-  select.appendChild(option);
-  select.appendChild(option1);
-
-  titleDiv.appendChild(cirlce);
-  titleDiv.appendChild(h2);
-  titleDiv.appendChild(para);
-  taskContainer2.appendChild(todoList);
 }
 
-// addTaskList(
-//   containerItems[0].title,
-//   containerItems[0].color,
-//   containerItems[0].count
-// );
+const listItems = document.createElement("div");
+listItems.setAttribute("class", "listItems");
 
+function lists(title, date, state) {
+  const cal = document.querySelector(`#${state}`);
+
+  const listItem = document.createElement("div");
+  listItem.setAttribute("class", "listItem");
+  listItems.appendChild(listItem);
+
+  const listItemP = document.createElement("p");
+  listItemP.innerText = title;
+
+  const dateItemP = document.createElement("p");
+  dateItemP.innerText = date;
+
+  const select = document.createElement("select");
+
+  containerItems.map((arr) => {
+    let option = document.createElement("option");
+    option.innerText = arr.id;
+    select.appendChild(option);
+  });
+
+  const img = document.createElement("img");
+  img.setAttribute("src", "./trash.svg");
+
+  cal.appendChild(listItem);
+
+  listItem.appendChild(listItemP);
+  listItem.appendChild(dateItemP);
+  listItem.appendChild(select);
+  listItem.appendChild(img);
+}
 containerItems.map((item) => {
   addTaskList(item.title, item.color, item.count, item.id);
 });
 
-// todoData.map((item) => {
-//   addTaskList(item.title, item.state, item.date);
-// });
+todoData.map((item) => {
+  lists(item.title, item.date, item.state);
+});
+inData.map((item) => {
+  lists(item.title, item.date, item.state);
+});
+doneData.map((item) => {
+  lists(item.title, item.date, item.state);
+});
+blockedData.map((item) => {
+  lists(item.title, item.date, item.state);
+});
